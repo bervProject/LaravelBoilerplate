6d6ea653de748b89c1061456e2d6ca7d6ef6f377
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2019-02-02 21:46:25 +0700

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <link href="{{asset('css/bulma.min.css')}}" rel="stylesheet" type="text/css">
+
+    </head>
+    <body>
+        <section class="section">
+            <div class="container">
+                <h1 class="title">
+                    Hello World!
+                </h1>
+                <h2 class="subtitle">
+                    Laravel
+                </h2>
+            </div>
+        </section>
+    </body>
+</html>
