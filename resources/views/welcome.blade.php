28d3d970ec7bb014ca668dd0132892e5a5e4e3ff
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-05-04 11:40:59 +0700

    Update to add auth vue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,6 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-        <link href="{{asset('css/bulma.min.css')}}" rel="stylesheet" type="text/css">
-
-    </head>
-    <body>
+@section('content')
         <section class="section">
             <div class="container">
                 <h1 class="title">
@@ -22,5 +11,4 @@
                 </h2>
             </div>
         </section>
-    </body>
-</html>
+@endsection
